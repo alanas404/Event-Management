e0df5b869e511ea2c161e003ec7695d40a68b30e
alanas404 <al.anas404040@gmail.com>
2024-11-25 01:11:42 +0600

    update

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Event::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         $event = Event::create([
+           ... $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time'
+           ]),
+           'user_id' => 1
+        ]);
+
+        return $event;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event)
+    {
+        return $event;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,Event $event)
+    {
+        $event->update($request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+           ]));
+
+        return $event;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event)
+    {
+        $event->delete();
+
+        return response(status:204);
+
+        // return response()->json([
+        //     'message' => 'Event deleted successfully'
+        // ]);
+    }
+}
